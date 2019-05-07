309c1b50c11ae5a1b90fb290122ad49d1d374e0f
Nekwan Luke <contact@nekwanluke.com>
2019-05-07 00:52:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target')
+}
+  
+function increaseRankBy(n) {
+  const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list') 
+
+  for (let i = 0; i < upRank.length; i++){
+    upRank[i].innerHTML =((parseInt(upRank[i].innerHTML)) + n)
+  }
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
 
